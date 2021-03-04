92e9930aa2afc574a166cff34828220a3e4f31d5
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-03-03 21:17:11 -0800

    Cube constants.

---
+++ rubiks/cube2x2x2.py
@@ -0,0 +1,40 @@
+#
+#  Rubiks
+#  Python Rubik's Cube module.
+#  Copyright Patrick Huang 2021
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+WHITE:   int = 0
+YELLOW:  int = 1
+BLUE:    int = 2
+GREEN:   int = 3
+RED:     int = 4
+ORANGE:  int = 5
+
+MOVE_R:  int = 6
+MOVE_RP: int = 7
+MOVE_L:  int = 8
+MOVE_LP: int = 9
+
+MOVE_F:  int = 10
+MOVE_FP: int = 11
+MOVE_B:  int = 12
+MOVE_BP: int = 13
+
+MOVE_T:  int = 14
+MOVE_TP: int = 15
+MOVE_B:  int = 16
+MOVE_BP: int = 17
